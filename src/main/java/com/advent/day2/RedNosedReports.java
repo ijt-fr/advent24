3b4b7f15d1b6f742327af90e76f20d9b64b1ec6d
Isaac Taylor <isaac.taylor@forgerock.com>
2024-12-02 09:10:07 +0000

    Some code cleanup.

--- src/main/java/com/advent/day2/RedNosedReports.java
+++ src/main/java/com/advent/day2/RedNosedReports.java
@@ -18,16 +18,17 @@ public class RedNosedReports extends Puzzle {
 
     @Override
     public Object computePart2(List<String> input) {
-        Map<List<Integer>, Boolean> safeReports = input.stream()
+        Map<List<Integer>, Boolean> reports = input.stream()
                 .map(RedNosedReports::parseReport)
                 .collect(Collectors.toMap(report -> report, RedNosedReports::isSafe));
 
-        long safeSubreports = safeReports.entrySet().stream()
+        long safeUnsafeReportCount = reports.entrySet().stream()
                 .filter(entry -> !entry.getValue())
                 .map(Map.Entry::getKey)
                 .filter(report -> {
                     for (int i = 0; i < report.size(); i++) {
                         List<Integer> subReport = new ArrayList<>(report);
+                        //noinspection SuspiciousListRemoveInLoop
                         subReport.remove(i);
                         if (isSafe(subReport)) {
                             return true;
@@ -36,24 +37,19 @@ public class RedNosedReports extends Puzzle {
                     return false;
                 }).count();
 
-        return safeReports.values().stream().filter(val -> val).count() + safeSubreports;
+        return reports.values().stream().filter(safe -> safe).count() + safeUnsafeReportCount;
     }
 
     private static boolean isSafe(List<Integer> report) {
-
         Integer first = report.get(0);
         Integer second = report.get(1);
         boolean increasing = second > first;
         for (int i = 0; i < report.size() - 1; i++) {
             var current = report.get(i);
             var next = report.get(i + 1);
-            var diff = Math.abs(next - current);
-            if (diff < 1 || diff > 3) {
-                return false;
-            }
-            if (increasing && next < current) {
-                return false;
-            } else if (!increasing && next > current) {
+            var diff = next - current;
+            var mod = Math.abs(diff);
+            if (mod < 1 || mod > 3 || (increasing && diff < 0) || (!increasing && diff > 0)) {
                 return false;
             }
         }
