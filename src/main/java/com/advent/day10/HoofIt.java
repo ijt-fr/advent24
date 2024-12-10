7be8dd28540dbf5a11ff516b115f179a0c64527a
Isaac Taylor <isaac.taylor@forgerock.com>
2024-12-10 10:52:42 +0000

    Tidy up day 10.

--- src/main/java/com/advent/day10/HoofIt.java
+++ src/main/java/com/advent/day10/HoofIt.java
@@ -5,10 +5,9 @@ import static com.advent.day6.Direction.NORTH;
 import static com.advent.day6.Direction.SOUTH;
 import static com.advent.day6.Direction.WEST;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,16 +20,16 @@ import com.advent.util.Vector2;
 
 public class HoofIt extends Puzzle {
     private Grid<Integer> map;
-    private Map<Vector2, Set<Vector2>> trailheads;
+    private Set<Vector2> trailheads;
 
     @Override
     public void parseInput(List<String> lines) {
-        trailheads = new HashMap<>();
+        trailheads = new HashSet<>();
         map = Grid.ofInts(InputUtils.toIntMatrix(lines));
         for (int x = 0; x < map.maxX(); x++) {
             for (int y = 0; y < map.maxY(); y++) {
                 if (map.get(x, y) == 0) {
-                    trailheads.put(new Vector2(x, y), new HashSet<>());
+                    trailheads.add(new Vector2(x, y));
                 }
             }
         }
@@ -38,13 +37,12 @@ public class HoofIt extends Puzzle {
 
     @Override
     public Object computePart1() {
-        trailheads.keySet().forEach(trailhead -> {
-            trailheads.put(trailhead,
-                    Stream.concat(Stream.concat(findEnds(NORTH, trailhead, 0), findEnds(SOUTH, trailhead, 0)),
-                                    Stream.concat(findEnds(EAST, trailhead, 0), findEnds(WEST, trailhead, 0)))
-                            .collect(Collectors.toSet()));
-        });
-        return trailheads.values().stream().map(Set::size).reduce(Integer::sum).get();
+        return trailheads.stream().map(trailhead ->  Arrays.stream(Direction.values())
+                                                             .flatMap(d -> findEnds(d, trailhead, 0))
+                                                             .collect(Collectors.toSet()))
+                       .map(Set::size)
+                       .reduce(Integer::sum)
+                       .orElse(0);
     }
 
     private Stream<Vector2> findEnds(Direction direction, Vector2 position, int currentHeight) {
@@ -57,8 +55,7 @@ public class HoofIt extends Puzzle {
             if (newHeight == 9) {
                 return Stream.of(newPosition);
             }
-            return Stream.concat(Stream.concat(findEnds(NORTH, newPosition, newHeight), findEnds(SOUTH, newPosition, newHeight)),
-                    Stream.concat(findEnds(EAST, newPosition, newHeight), findEnds(WEST, newPosition, newHeight)));
+            return Arrays.stream(Direction.values()).flatMap(d -> findEnds(d, newPosition, newHeight));
         }
         return Stream.empty();
     }
@@ -70,10 +67,11 @@ public class HoofIt extends Puzzle {
 
     @Override
     public Object computePart2() {
-        return trailheads.keySet().stream().map(trailhead -> trailCount(NORTH, trailhead, 0)
-                                                                     + trailCount(SOUTH, trailhead, 0)
-        + trailCount(EAST, trailhead, 0) + trailCount(WEST, trailhead, 0))
-                       .reduce(Integer::sum).get();
+        return trailheads.stream()
+                       .map(trailhead -> Arrays.stream(Direction.values())
+                                                 .map(direction -> trailCount(direction, trailhead, 0))
+                                                 .reduce(Integer::sum).orElse(0))
+                       .reduce(Integer::sum).orElse(0);
     }
 
     private Integer trailCount(Direction direction, Vector2 position, int currentHeight) {
@@ -86,8 +84,9 @@ public class HoofIt extends Puzzle {
             if (newHeight == 9) {
                 return 1;
             }
-            return trailCount(NORTH, newPosition, newHeight) + trailCount(SOUTH, newPosition, newHeight)
-                           + trailCount(EAST, newPosition, newHeight) + trailCount(WEST, newPosition, newHeight);
+            return Arrays.stream(Direction.values())
+                           .map(d -> trailCount(d, newPosition, newHeight))
+                           .reduce(Integer::sum).orElse(0);
         }
         return 0;
     }
