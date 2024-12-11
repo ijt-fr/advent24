071b39da8f7bc92d6eb8dede8de1a9a84bde223a
Isaac Taylor <isaac.taylor@forgerock.com>
2024-12-11 10:28:54 +0000

    Day 11 part 2.

--- src/main/java/com/advent/day11/PlutonianPebbles.java
+++ src/main/java/com/advent/day11/PlutonianPebbles.java
@@ -2,27 +2,29 @@ package com.advent.day11;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
 import com.advent.Puzzle;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 public class PlutonianPebbles extends Puzzle {
 
-    Stream<Stone> stones;
+    Stream<Long> stones;
+    LoadingCache<CacheKey, Long> memoCache = CacheBuilder.newBuilder()
+                                                     .build(CacheLoader.from(this::blink));
 
     @Override
     public void parseInput(List<String> lines) {
         stones = Arrays.stream(lines.getFirst().split(" "))
-                .map(Long::parseLong)
-                .map(Stone::new);
+                .map(Long::parseLong);
     }
 
     @Override
     public Object computePart1() {
-        for (int i = 0; i < 25; i++) {
-            stones = stones.flatMap(Stone::blink);
-        }
-        return stones.count();
+        return stones.map(l -> blink(l, 25)).reduce(Long::sum).orElse(0L);
     }
 
     @Override
@@ -32,41 +34,63 @@ public class PlutonianPebbles extends Puzzle {
 
     @Override
     public Object computePart2() {
-        for (int i = 0; i < 75; i++) {
-            stones = stones.flatMap(Stone::blink);
-        }
-        return stones.count();
+        return stones.map(l -> blink(l, 75)).reduce(Long::sum).orElse(0L);
     }
 
     @Override
     public Object part2Answer() {
-        return null;
+        return 274229228071551L;
     }
 
-    record Stone(Long number) {
+    private Long blink(Long l, int count) {
+        try {
+            return memoCache.get(new CacheKey(l, count));
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        Stream<Stone> blink() {
+    private long blink(CacheKey key) {
+        int newCount = key.count() - 1;
+        long number = key.number();
+        if (newCount == 0) {
             if (number == 0) {
-                return Stream.of(new Stone(1L));
+                return 1;
             }
-            var digits = digits();
+            if (digits(number) % 2 == 0) {
+                return 2;
+            }
+            return 1;
+        }
+        try {
+            if (number == 0) {
+                return memoCache.get(new CacheKey(1L, newCount));
+            }
+            var digits = digits(number);
             if (digits % 2 == 0) {
-                long multiple = (long) Math.pow(10, digits / 2);
-                return Stream.of(new Stone(upper(multiple)), new Stone(lower(multiple)));
+                long multiple = (long) Math.pow(10, (double) digits / 2);
+                return memoCache.get(new CacheKey(upper(number, multiple), newCount))
+                               + memoCache.get(new CacheKey(lower(number, multiple), newCount));
             }
-            return Stream.of(new Stone(number * 2024L));
+            return memoCache.get(new CacheKey(number * 2024L, newCount));
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        private int digits() {
-            return (int) (Math.log10(Math.abs(number)) + 1);
-        }
+    private int digits(long number) {
+        return (int) (Math.log10(Math.abs(number)) + 1);
+    }
 
-        private long upper(long multiple) {
-            return number / multiple;
-        }
+    private long upper(long number, long multiple) {
+        return number / multiple;
+    }
 
-        private long lower(long multiple) {
-            return number % multiple;
-        }
+    private long lower(long number, long multiple) {
+        return number % multiple;
+    }
+
+    record CacheKey(Long number, int count) {
+
     }
 }
--- src/test/java/com/advent/day11/PlutonianPebblesTest.java
+++ src/test/java/com/advent/day11/PlutonianPebblesTest.java
@@ -8,9 +8,7 @@ import com.advent.BasePuzzleTest;
 
 public class PlutonianPebblesTest extends BasePuzzleTest<PlutonianPebbles> {
 
-    private static final String EXAMPLE = "0 1 10 99 999";
-
-    private static final String EXAMPLE_2 = "125 17";
+    private static final String EXAMPLE = "125 17";
 
     @Override
     public PlutonianPebbles getPuzzle() {
@@ -19,11 +17,11 @@ public class PlutonianPebblesTest extends BasePuzzleTest<PlutonianPebbles> {
 
     @Override
     public Stream<Arguments> part1Samples() {
-        return Stream.of(Arguments.of(EXAMPLE_2, 55312L));
+        return Stream.of(Arguments.of(EXAMPLE, 55312L));
     }
 
     @Override
     public Stream<Arguments> part2Samples() {
-        return Stream.empty();
+        return Stream.of(Arguments.of(EXAMPLE, 65601038650482L));
     }
 }
